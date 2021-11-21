a7a9d2a31c50c930ed75143b6c9bf21a817e5335
ZelGaDisA <zelasas@mail.ru>
2021-11-21 23:02:20 +0300

    fix css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,29 @@
 import React from "react";
+import ss from './Navbar.module.css';
+
+// let ss = {
+//     'nav' : 'Navbar_nav_3ou9Q',
+//     'item' : 'Navbar_item_3qaF3'
+// };
 
 
 const Navigation = () => {
     return (
             <div>
-                <nav className='nav'>
-                    <div>
+                <nav className={ss.nav}>
+                    <div className={ss.item}>
                         <a>Profile</a>
                     </div>
-                    <div>
+                    <div className={ss.item}>
                         <a>News</a>
                     </div>
-                    <div>
+                    <div className='item'>
                         <a>Messages</a>
                     </div>
-                    <div>
+                    <div className='item'>
                         <a>Muzik</a>
                     </div>
-                    <div>
+                    <div className='item'>
                         <a>Settings</a>
                     </div>
                 </nav>
