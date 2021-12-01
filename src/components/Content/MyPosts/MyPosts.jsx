8a2250f6b56cfa2a4694188c5424a2a51beb59ed
ZelGaDisA <zelasas@mail.ru>
2021-12-01 20:36:26 +0300

    fix action creators

--- src/components/Content/MyPosts/MyPosts.jsx
+++ src/components/Content/MyPosts/MyPosts.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ss from './MyPosts.module.css';
 import Post from './Post/Post';
-import updateNewPostTextActionCreator from '../../../redux/state';
-import addPostActionCreator from '../../../redux/state';
+import {updateNewPostTextActionCreator} from '../../../redux/state';
+import {addPostActionCreator} from '../../../redux/state';
 
 const MyPosts = (props) => {
 
