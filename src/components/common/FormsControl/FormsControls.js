ce4c2a5ce7a0a34865e1e775499d23b3b32f58d5
ZelGaDisA <zelasas@mail.ru>
2022-01-11 19:51:02 +0300

    add css on FormKontrols

--- src/components/common/FormsControl/FormsControls.js
+++ src/components/common/FormsControl/FormsControls.js
@@ -1,10 +1,12 @@
 import React from "react"
+import s from "./FormsControls.module.css"
 
 
 export const Textarea = ({input, meta, ...props}) => {
     return (
-        <div>
+        <div className={s.formKontrol + "" + s.error}>
             <textarea {...input} {...props}/>
+            <span>{"some error"}</span>
         </div>
     )
 }
