309e7215143d6cb8f0c60b4dbb69b4800ae3863b
ZelGaDisA <zelasas@mail.ru>
2022-01-26 17:17:05 +0300

    fix api add reduserApp

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,112 @@
+
 import * as axios from "axios"
 
-
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        "API-KEY": "718ec874-1ab3-4071-8420-87750c11fc6f"
-    }
-});
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "718ec874-1ab3-4071-8420-87750c11fc6f"
+  }
+})
+
 
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => {
-                return response.data;
-            });
-    },
-    follow(userId) {
-        return instance.post(`follow/${userId}`)
-    },
-    unfollow(userId) {
-        return instance.delete(`follow/${userId}`)
-    },
-    getProfile(userId) {
-        console.warn('Obsolete method. Use profileAPI object')
-        return profileAPI.getProfile(userId)
-    }
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+  },
+  follow(userId) {
+    return instance.post(`follow/${userId}`, {})
+  },
+  unfollow(userId) {
+    return instance.delete(`follow/${userId}`)
+  },
+  getProfile(userId) {
+    console.warn("Obsolete method. Please use profileAPI object")
+    return profileAPI.getProfile(userId)
+  }
 }
 
 export const profileAPI = {
-    getProfile(userId) {
-        return instance.get(`profile/` + userId)
-    },
-    getStatus(userId) {
-        return instance.get(`profile/status/` + userId)
-    },
-    unpdateStatus(status) {
-        return instance.put(`profile/status`, { status: status })
-    }
+  getProfile(userId) {
+    return instance.get(`profile/${userId}`)
+  },
+  getStatus(userId) {
+    return instance.get(`profile/status/${userId}`)
+  },
+  updateStatus(status) {
+    return instance.put(`profile/status`, { status: status })
+  },
+  savePhoto(photoFile) {
+    const formData = new FormData()
+    formData.append("image", photoFile)
+    return instance.put(`profile/photo`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    })
+  }
 }
-
 
 export const authAPI = {
-    me() {
-        return instance.get(`auth/me`)
-    },
-    login (email, password, rememberMe = false) {
-        return instance.post(`auth/login`, { email, password, rememberMe })
-    },
-    logout () {
-        return instance.delete(`auth/login`)
-    },
+  me() { return instance.get(`auth/me`) },
+  login(email, password, rememberMe = false) { return instance.post(`auth/login`, { email, password, rememberMe }) },
+  logout() { return instance.delete(`auth/login`) },
 }
+
+
+
+
+// import * as axios from "axios"
+
+
+// const instance = axios.create({
+//     withCredentials: true,
+//     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+//     headers: {
+//         "API-KEY": "718ec874-1ab3-4071-8420-87750c11fc6f"
+//     }
+// });
+
+// export const usersAPI = {
+//     getUsers(currentPage = 1, pageSize = 10) {
+//         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+//             .then(response => {
+//                 return response.data;
+//             });
+//     },
+//     follow(userId) {
+//         return instance.post(`follow/${userId}`)
+//     },
+//     unfollow(userId) {
+//         return instance.delete(`follow/${userId}`)
+//     },
+//     getProfile(userId) {
+//         console.warn('Obsolete method. Use profileAPI object')
+//         return profileAPI.getProfile(userId)
+//     }
+// }
+
+// export const profileAPI = {
+//     getProfile(userId) {
+//         return instance.get(`profile/` + userId)
+//     },
+//     getStatus(userId) {
+//         return instance.get(`profile/status/` + userId)
+//     },
+//     unpdateStatus(status) {
+//         return instance.put(`profile/status`, { status: status })
+//     }
+// }
+
+
+// export const authAPI = {
+//     me() {
+//         return instance.get(`auth/me`)
+//     },
+//     login (email, password, rememberMe = false) {
+//         return instance.post(`auth/login`, { email, password, rememberMe })
+//     },
+//     logout () {
+//         return instance.delete(`auth/login`)
+//     },
+// }
