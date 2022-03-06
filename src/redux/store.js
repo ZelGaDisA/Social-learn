23d624affdfd26c0aa220a014f5310d1c9e0f762
ZelGaDisA <zelasas@mail.ru>
2022-03-06 20:07:15 +0300

    fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,63 +1,55 @@
-import profileReduser from "./content-reduser";
-import dialogsReduser from "./dialogs-reduser";
-import sitebarReduser from "./sitebar-reduser";
+import dialogsReducer from "./dialogsReducer"
+import profileReducer from "./profileReducer"
 
 let store = {
-    _state: {
-        profilePage: {
-            posts: [
-                { id: 1, message: 'Hi Jack', likesCount: 12 },
-                { id: 2, message: 'Bye Jack', likesCount: 14 },
-                { id: 3, message: 'Hello Jack', likesCount: 22 },
-                { id: 4, message: 'Fuck Jack', likesCount: 42 },
-                { id: 5, message: 'Me Jack', likesCount: 17 },
-                { id: 6, message: 'your jack', likesCount: 11 }
-            ],
-            newPostText: 'it-kama'
-        },
-        dialogsPage: {
-            messages: [
-                { id: 1, message: 'Hi Jack' },
-                { id: 2, message: 'Bye Jack' },
-                { id: 3, message: 'Hello Jack' },
-                { id: 4, message: 'Fuck Jack' },
-                { id: 5, message: 'Me Jack' },
-                { id: 6, message: 'your jack' }
-            ],
-            dialogs: [
-                { id: 1, name: 'Dimych' },
-                { id: 2, name: 'Chyvak' },
-                { id: 3, name: 'Evgen' },
-                { id: 4, name: 'Sveta' },
-                { id: 5, name: 'Saha' },
-                { id: 6, name: 'Valeryiu' }
-            ],
-            newMessageBody: ""
-        },
-        sitebar: {}
+  _state: {
+    profilePage: {
+      posts: [
+        {id: 1, likesCount: 1, text: "any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. any text u wanna write. "},
+        {id: 2, likesCount: 15, text: "lorem lorem blablabla"},
+        {id: 3, likesCount: 5, text: "SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED SO BORED "},
+      ],
+      newPostText: ""
     },
-    _callSubscriber() {
-        console.log('state changed')
-    },
-
-    getState() {
-        return this._state;
-    },
-    subscribe(observer) {
-        this._callSubscriber = observer;
-    },
-
-    dispatch(action) {
-
-        this._state.profiletPage = profileReduser(this._state.profilePage, action);
-        this._state.dialogsPage = dialogsReduser(this._state.dialogsPage, action);
-        this._state.sitebar = sitebarReduser(this._state.sitebar, action);
-
-        this._callSubscriber(this._state);
+    dialogsPage: {
+      dialogs: [
+        {id: 1, name: 'JavaScript'},
+        {id: 2, name: 'GitHub'},
+        {id: 3, name: 'React'},
+        {id: 4, name: 'Redux'},
+        {id: 5, name: 'Google'},
+        {id: 6, name: 'YouTube'},
+      ],
+      messages: [
+        {id: 1, message: 'Hey, how are you? =)'},
+        {id: 2, message: 'Hello world'},
+        {id: 3, message: 'Hi'},
+        {id: 4, message: 'bb gl hf'},
+        {id: 5, message: 'cya'},
+        {id: 6, message: 'you welcome'},
+      ],
+      newMessageBody: ""
     }
+  },
+  _callSubscriber() {
+    console.log('state changed');
+  },
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer //observer pattern
+  },
+
+  dispatch(action) {
+
+    this._state.profilePage = profileReducer(this._state.profilePage, action)
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
+
+    this._callSubscriber(this._state)
+    
+  }
 }
 
-
-export default store;
-
-window.store = store;
+export default store
+window.store = store
